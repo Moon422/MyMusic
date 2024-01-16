5be0ded3a13818d36e227dc7c6463ee74f9abf24
Mahfuzur Rahman <90880886+Moon422@users.noreply.github.com>
2024-01-16 19:07:48 +0600

    added  Microsoft.AspNetCore.Authentication.JwtBearer

--- Backend/Program.cs
+++ Backend/Program.cs
@@ -32,6 +32,7 @@ public class Program
         // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
         builder.Services.AddEndpointsApiExplorer();
         builder.Services.AddSwaggerGen();
+        // builder.Services.AddAuthentication();
 
         var app = builder.Build();
 
