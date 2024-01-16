1c428a586bf9497dcb4729011d8cbfed029d47e1
Mahfuzur Rahman <90880886+Moon422@users.noreply.github.com>
2024-01-16 19:39:38 +0600

    Added profile type

--- Backend/Migrations/20240116133932_Added profile type.Designer.cs
+++ Backend/Migrations/20240116133932_Added profile type.Designer.cs
@@ -2,6 +2,7 @@
 using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using MyMusic.Backend.Services;
 
@@ -10,9 +11,11 @@ using MyMusic.Backend.Services;
 namespace Backend.Migrations
 {
     [DbContext(typeof(MusicDB))]
-    partial class MusicDBModelSnapshot : ModelSnapshot
+    [Migration("20240116133932_Added profile type")]
+    partial class Addedprofiletype
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ Backend/Migrations/20240116133932_Added profile type.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Backend.Migrations
+{
+    /// <inheritdoc />
+    public partial class Addedprofiletype : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "ProfileTypes",
+                table: "Profiles",
+                type: "int",
+                nullable: false,
+                defaultValue: 2);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "ProfileTypes",
+                table: "Profiles");
+        }
+    }
+}
