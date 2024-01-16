8bf55b91feb2c5a7f4c3931ddfffc069c4583d0c
Mahfuzur Rahman <90880886+Moon422@users.noreply.github.com>
2024-01-16 21:24:10 +0600

    Refresh token points to user

---
+++ Backend/Migrations/20240116152400_Refresh token points to user.cs
@@ -0,0 +1,62 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Backend.Migrations
+{
+    /// <inheritdoc />
+    public partial class Refreshtokenpointstouser : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_RefreshTokens_Profiles_ProfileId",
+                table: "RefreshTokens");
+
+            migrationBuilder.RenameColumn(
+                name: "ProfileId",
+                table: "RefreshTokens",
+                newName: "UserId");
+
+            migrationBuilder.RenameIndex(
+                name: "IX_RefreshTokens_ProfileId",
+                table: "RefreshTokens",
+                newName: "IX_RefreshTokens_UserId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_RefreshTokens_Users_UserId",
+                table: "RefreshTokens",
+                column: "UserId",
+                principalTable: "Users",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_RefreshTokens_Users_UserId",
+                table: "RefreshTokens");
+
+            migrationBuilder.RenameColumn(
+                name: "UserId",
+                table: "RefreshTokens",
+                newName: "ProfileId");
+
+            migrationBuilder.RenameIndex(
+                name: "IX_RefreshTokens_UserId",
+                table: "RefreshTokens",
+                newName: "IX_RefreshTokens_ProfileId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_RefreshTokens_Profiles_ProfileId",
+                table: "RefreshTokens",
+                column: "ProfileId",
+                principalTable: "Profiles",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+    }
+}
