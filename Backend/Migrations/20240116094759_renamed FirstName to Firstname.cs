0a58f1fac2cbd9e7b70ba97046d3855d69c9e621
Mahfuzur Rahman <90880886+Moon422@users.noreply.github.com>
2024-01-16 15:48:05 +0600

    renamed FirstName to Firstname

---
+++ Backend/Migrations/20240116094759_renamed FirstName to Firstname.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Backend.Migrations
+{
+    /// <inheritdoc />
+    public partial class renamedFirstNametoFirstname : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "FirstName",
+                table: "Profiles",
+                newName: "Firstname");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "Firstname",
+                table: "Profiles",
+                newName: "FirstName");
+        }
+    }
+}
