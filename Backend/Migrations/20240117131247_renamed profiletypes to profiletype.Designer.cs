2ccc028bee49780c00f5465c2f296864924e31c2
Mahfuzur Rahman <90880886+Moon422@users.noreply.github.com>
2024-01-17 19:12:55 +0600

    renamed profiletypes to profiletype

--- Backend/Migrations/20240117131247_renamed profiletypes to profiletype.Designer.cs
+++ Backend/Migrations/20240117131247_renamed profiletypes to profiletype.Designer.cs
@@ -2,6 +2,7 @@
 using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using MyMusic.Backend.Services;
 
@@ -10,9 +11,11 @@ using MyMusic.Backend.Services;
 namespace Backend.Migrations
 {
     [DbContext(typeof(MusicDB))]
-    partial class MusicDBModelSnapshot : ModelSnapshot
+    [Migration("20240117131247_renamed profiletypes to profiletype")]
+    partial class renamedprofiletypestoprofiletype
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
