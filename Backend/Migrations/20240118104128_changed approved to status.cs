1a95b83cfa59d5f75726b9b3aa0a20934598ee68
Mahfuzur Rahman <90880886+Moon422@users.noreply.github.com>
2024-01-18 16:41:35 +0600

    changed approved to status

---
+++ Backend/Migrations/20240118104128_changed approved to status.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Backend.Migrations
+{
+    /// <inheritdoc />
+    public partial class changedapprovedtostatus : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Approved",
+                table: "ArtistUpgradeRequests");
+
+            migrationBuilder.AddColumn<int>(
+                name: "Status",
+                table: "ArtistUpgradeRequests",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Status",
+                table: "ArtistUpgradeRequests");
+
+            migrationBuilder.AddColumn<bool>(
+                name: "Approved",
+                table: "ArtistUpgradeRequests",
+                type: "tinyint(1)",
+                nullable: false,
+                defaultValue: false);
+        }
+    }
+}
