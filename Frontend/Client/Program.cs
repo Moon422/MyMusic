605ef145d828d980abff92734def0adfcf9f2e0e
Mahfuzur Rahman <90880886+Moon422@users.noreply.github.com>
2024-01-25 21:17:43 +0600

    adding howler.js

--- Frontend/Client/Program.cs
+++ Frontend/Client/Program.cs
@@ -1,3 +1,4 @@
+using Howler.Blazor.Components;
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 
@@ -12,6 +13,8 @@ public class Program
         builder.RootComponents.Add<HeadOutlet>("head::after");
 
         builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+        builder.Services.AddScoped<IHowl, Howl>();
+        builder.Services.AddScoped<IHowlGlobal, HowlGlobal>();
 
         await builder.Build().RunAsync();
     }
