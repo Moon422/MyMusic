97712f0997484eec037d4b61228111927331fad3
Mahfuzur Rahman <90880886+Moon422@users.noreply.github.com>
2024-01-26 09:19:32 +0600

    separated code to playback manager

--- Frontend/Helpers/PlaybackInfo.cs
+++
@@ -1,9 +0,0 @@
-namespace MyMusic.Frontend.Helpers;
-
-public class PlaybackInfo
-{
-    public PlaybackStatus PlaybackStatus { get; set; } = PlaybackStatus.Stopped;
-    public int Volume { get; set; }
-    public bool Muted { get; set; }
-    public int CurentTrackId { get; set; }
-}
---
+++ Frontend/Helpers/PlaybackManager.cs
@@ -0,0 +1,38 @@
+using System.Net.Http.Json;
+using Howler.Blazor.Components;
+using MyMusic.ViewModels;
+
+namespace MyMusic.Frontend.Helpers;
+
+public class PlaybackManager
+{
+    private const string BASE_URL = "https://coreapi.shadhinmusic.com/api/v5";
+
+    private readonly IHowl howl;
+    private readonly IHowlGlobal globalHowl;
+    private readonly HttpClient httpClient;
+
+    public PlaybackStatus PlaybackStatus { get; set; } = PlaybackStatus.Stopped;
+    public int Volume { get; set; }
+    public bool Muted { get; set; }
+    public int CurentTrackId { get; set; }
+    public TimeSpan TotalTime { get; set; }
+
+    private async Task ChangeTrack(string filename = "AudioMainFile/Aalo_Tahsan.mp3")
+    {
+        if (PlaybackStatus != PlaybackStatus.Playing)
+        {
+            PlaybackStatus = PlaybackStatus.Loading;
+
+            using var requestMessage = new HttpRequestMessage(HttpMethod.Get, $"{BASE_URL}/streaming/getpth?ptype=S&type=null&ttype=null&name={filename}");
+            requestMessage.Headers.Add("User-Agent", "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:100.0) Gecko/20100101 Firefox/100.0");
+            var response = await httpClient.SendAsync(requestMessage);
+
+            if (response.IsSuccessStatusCode)
+            {
+                var trackResponse = await response.Content.ReadFromJsonAsync<TrackBlobResponse>();
+                CurentTrackId = await howl.Play(trackResponse!.Data);
+            }
+        }
+    }
+}
