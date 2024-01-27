73d37b1305a62fc8c347008583442bd87d36da20
Mahfuzur Rahman <90880886+Moon422@users.noreply.github.com>
2024-01-27 18:32:18 +0600

    sqlite12

--- Backend/Migrations/20240127123211_sqlite12.Designer.cs
+++ Backend/Migrations/20240127123211_sqlite12.Designer.cs
@@ -2,6 +2,7 @@
 using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using MyMusic.Backend.Services;
 
@@ -10,9 +11,11 @@ using MyMusic.Backend.Services;
 namespace Backend.Migrations
 {
     [DbContext(typeof(MusicDB))]
-    partial class MusicDBModelSnapshot : ModelSnapshot
+    [Migration("20240127123211_sqlite12")]
+    partial class sqlite12
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ Backend/Migrations/20240127123211_sqlite12.cs
@@ -0,0 +1,51 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Backend.Migrations
+{
+    /// <inheritdoc />
+    public partial class sqlite12 : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "AlbumArtist");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "AlbumArtist",
+                columns: table => new
+                {
+                    AlbumsId = table.Column<int>(type: "int", nullable: false),
+                    ArtistsId = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_AlbumArtist", x => new { x.AlbumsId, x.ArtistsId });
+                    table.ForeignKey(
+                        name: "FK_AlbumArtist_Albums_AlbumsId",
+                        column: x => x.AlbumsId,
+                        principalTable: "Albums",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_AlbumArtist_Artists_ArtistsId",
+                        column: x => x.ArtistsId,
+                        principalTable: "Artists",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                })
+                .Annotation("MySql:CharSet", "utf8mb4");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_AlbumArtist_ArtistsId",
+                table: "AlbumArtist",
+                column: "ArtistsId");
+        }
+    }
+}
